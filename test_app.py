ca711087842cce910bc226beb8eb7d2e13d3008f
lalu <mathieu@digithink.ch>
2016-05-25 18:35:47 +0200

    First commit test file

---
+++ test_app.py
@@ -0,0 +1,37 @@
+from app import app
+import unittest
+
+class FlaskAppTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_home_data(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the response data
+        self.assertEqual(result.data, "Hi ! I\'m a Flask application runned in a Docker container.")
