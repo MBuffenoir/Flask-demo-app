5cce4ac2bd62889df9c9a2d05ac36640d43c338e
lalu <mathieu@digithink.ch>
2016-07-28 14:25:42 +0200

    Trying the whole process

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.debug = True
 
 @app.route('/')
 def main():
-    return 'Hi ! I\'m a Flask application.'
+    return 'Hi ! I\'m a Flask application. I was automatically tested and deployed using Travis'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
